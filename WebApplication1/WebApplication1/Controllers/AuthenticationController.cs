06157fb7128662e31791eec79b54f665f9fd0647
John-coder24 <john@eagleuptech.com>
2023-09-11 18:31:21 +0530

    Registeration and Role

---
+++ WebApplication1/WebApplication1/Controllers/AuthenticationController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using userManagment.Models;
+using userManagment.Models.SignUp;
+
+namespace userManagment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        public AuthenticationController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUserModel registerUser, string role)
+        {
+            // Check users Exist
+            var userExist = await _userManager.FindByEmailAsync(registerUser.Email);
+            if (userExist != null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, new Response
+                {
+                    Status = "Error", Message = "User Already Exit"
+                });
+            }
+
+            // Add the User in the Database
+            IdentityUser user = new()
+            {
+                Email = registerUser.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerUser.Username
+            };
+
+            if (await _roleManager.RoleExistsAsync(role)) {
+                var result = await _userManager.CreateAsync(user, registerUser.Password);
+                if (!result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, new Response
+                    {
+                        Status = "Error",
+                        Message = "User Failed to Create"
+                    });
+                }
+                // Add role to the user 
+                await _userManager.AddToRoleAsync(user, role);
+
+                return StatusCode(StatusCodes.Status200OK,
+                   new Response { Status = "Success", Message = "User Created Successfully" });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, 
+                    new Response { Status = "Success", Message = "This Role Doesnot Exist" });
+
+            }
+
+           
+           
+
+            // Assign a role.
+
+
+
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Migrations/20230908131341_RolesSeeded.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace userManagment.Migrations
+{
+    public partial class RolesSeeded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "9aa766cb-d53b-4957-98e4-11cd9efa1988", "3", "HR", "HR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "a0800f58-96dc-49c1-9b2f-3f9bc0ba90a8", "1", "Admin", "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "f1d9ae3e-34d0-430b-8a64-8f9a7d1e58fe", "2", "User", "User" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9aa766cb-d53b-4957-98e4-11cd9efa1988");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a0800f58-96dc-49c1-9b2f-3f9bc0ba90a8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f1d9ae3e-34d0-430b-8a64-8f9a7d1e58fe");
+        }
+    }
+}
